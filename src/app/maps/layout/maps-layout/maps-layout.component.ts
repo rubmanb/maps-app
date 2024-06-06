9a5e0595bd3df7532c7ddcf0d764bc3eb5e4d334
rubmanb <rubenm1386@gmail.com>
2024-06-06 16:53:04 +0200

    adding token and scripting to create env file. The token isn't uploading

--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   templateUrl: './maps-layout.component.html',
   styleUrl: './maps-layout.component.css'
 })
 export class MapsLayoutComponent {
-
+  map = L.map('map').setView([51.505, -0.09], 13);
 }
